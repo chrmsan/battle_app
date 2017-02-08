e4aa3e96a28074371f1485d7af3ef814189c1244
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-02-08 16:41:29 +0100

    Add get and post paths for player names

--- lib/my_app.rb
+++ lib/my_app.rb
@@ -3,7 +3,13 @@ require 'sinatra/base'
 class MyApp < Sinatra::Base
   
   get '/' do
-    'Hello MyApp!'
+    erb :index
+  end
+
+  post '/names' do
+    @player1 = params[:player_1]
+    @player2 = params[:player_2]
+    erb :play
   end
 
   run! if app_file == $0

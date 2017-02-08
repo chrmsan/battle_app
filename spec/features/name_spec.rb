775c3eabdef5212463bb8d6dac5dc41d6c90dd0b
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-02-08 16:32:22 +0100

    Add feature test requesting player names

---
+++ spec/features/name_spec.rb
@@ -0,0 +1,13 @@
+feature 'Adding player names' do
+
+	scenario 'A user add names to Player 1 and Player 2' do
+		visit '/'
+		fill_in 'Player 1', with: 'Johnny Bravo'
+		fill_in 'Player 2', with: 'Jason Bourne'
+		click_button 'Submit'
+		visit '/names'
+		expect(page).to have_content('Johnny Bravo')
+		expect(page).to have_content('Jason Bourne')
+	end
+
+end

3774698302b2c8bf1a5ee1b19e82f2443fcb7d98
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-02-10 17:55:18 +0100

    Add form in index.erb

--- spec/features/name_spec.rb
+++ spec/features/name_spec.rb
@@ -4,8 +4,8 @@ feature 'Adding player names' do
 
 	scenario 'A user add names to Player 1 and Player 2' do
 		visit '/'
-		fill_in 'Player 1', with: 'Johnny Bravo'
-		fill_in 'Player 2', with: 'Jason Bourne'
+		fill_in 'player_1', with: 'Johnny Bravo'
+		fill_in 'player_2', with: 'Jason Bourne'
 		click_button 'Submit'
 		visit '/names'
 		expect(page).to have_content('Johnny Bravo')

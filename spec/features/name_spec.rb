920f47b00c16d9d3d7e57ae0bb63c85a5d58fc2d
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-02-10 18:16:36 +0100

    Remove unecessary capybara element in test

--- spec/features/name_spec.rb
+++ spec/features/name_spec.rb
@@ -7,7 +7,6 @@ feature 'Adding player names' do
 		fill_in 'player_1', with: 'Johnny Bravo'
 		fill_in 'player_2', with: 'Jason Bourne'
 		click_button 'Submit'
-		visit '/names'
 		expect(page).to have_content('Johnny Bravo')
 		expect(page).to have_content('Jason Bourne')
 	end

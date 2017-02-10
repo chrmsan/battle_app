ba4ddead54565a4454baf7e551ff551c1e04239f
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-02-10 18:54:16 +0100

    Enabled sessions in controller and replaced instance variables in post route with sessions variables

--- lib/my_app.rb
+++ lib/my_app.rb
@@ -1,14 +1,16 @@
 require 'sinatra/base'
 
 class MyApp < Sinatra::Base
+
+  enable :sessions
   
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player1 = params[:player_1]
-    @player2 = params[:player_2]
+    sessions[:player_1] = params[:player_1]
+    sessions[:player_2] = params[:player_2]
     erb :play
   end
 

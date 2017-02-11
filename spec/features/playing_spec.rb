e6153e605226c832010fbc1e3b0aef45eb5aab80
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-02-11 16:55:02 +0100

    Refactor test

--- spec/features/playing_spec.rb
+++ spec/features/playing_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 feature 'game play' do
 	scenario 'Player 1 gets attack confirmation on Player 2' do
 		sign_in_and_play
-		click_button 'Player 1 Attack!'
-		expect(page).to have_content('Player 1 has attacked Player 2')
+		click_button 'Attack!'
+		expect(page).to have_content('Johnny Bravo has attacked Jason Bourne')
 	end
 end

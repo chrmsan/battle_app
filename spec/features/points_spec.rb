2a471ede5f5df98c6900186894cff5c8366d6865
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-02-11 15:54:41 +0100

    Implement web_helper method in relevant test

--- spec/features/points_spec.rb
+++ spec/features/points_spec.rb
@@ -3,10 +3,7 @@ require 'spec_helper'
 feature 'Adding player names' do
 
 	scenario 'A user add names to Player 1 and Player 2' do
-		visit '/'
-		fill_in 'player_1', with: 'Johnny Bravo'
-		fill_in 'player_2', with: 'Jason Bourne'
-		click_button 'Submit'
+		sign_in_and_play
 		expect(page).to have_content('Jason Bourne (100HP)')
 	end
 

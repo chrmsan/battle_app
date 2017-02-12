a339c95b03943eafae70898e3cc01ed54e878ae0
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-02-12 19:57:13 +0100

    Add test where player can inflicted damage through a method

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Player do
 	subject(:pikachu) {Player.new('Pikachu')}
+	subject(:bulbasaur) {Player.new('Bulbasaur')}
 
 	describe '#name' do
 		it 'returns the player name' do
@@ -12,6 +13,13 @@ describe Player do
 	describe '#hit_points' do
 		it 'returns the players HP' do
 			expect(pikachu.hitpoints).to eq(described_class::DEF_HP)
+		end
+	end
+
+	describe '#attack' do
+		it 'inflict damage to player who is being attacked' do
+			expect(bulbasaur).to receive(:receive_damage)
+			pikachu.attack(bulbasaur)
 		end
 	end
 end

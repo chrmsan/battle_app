ca9061d9756b05aa57b91141f6c59f92ebea8f3d
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-02-12 21:32:01 +0100

    Add test to link going from /attack to /play path

--- spec/features/playing_spec.rb
+++ spec/features/playing_spec.rb
@@ -10,6 +10,7 @@ feature 'game play' do
 	scenario 'Player 2 lose 10HP when attacked' do
 		sign_in_and_play
 		click_button 'Attack!'
+		click_link 'Return to Game'
 		expect(page).not_to have_content('Jason Bourne (100HP)')
 		expect(page).to have_content('Jason Bourne (90HP)')
 	end

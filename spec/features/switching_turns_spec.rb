3a0cb14e1bf56a3882cf866789909d5ea7511cf3
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-02-14 16:57:18 +0100

    Add features test for content when switching turns

--- spec/features/switching_turns_spec.rb
+++ spec/features/switching_turns_spec.rb
@@ -7,5 +7,13 @@ feature 'Switching turns ' do
 			expect(page).to have_content("Johnny Bravo's Turn")
 		end
 
+		scenario 'after player 1 has attacked' do
+			sign_in_and_play
+			sign_in_and_play
+			click_button 'Attack!'
+			click_link 'Return to Game'
+			expect(page).not_to have_content("Johnny Bravo's Turn")
+			expect(page).to have_content("Jason Bourne's Turn")
+		end
 	end
 end

3a61ad994ab01771416a8e7acfae4b2eb7d77867
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-02-14 16:50:36 +0100

    Change expect content in text wrt sign_in_and_play helper method

--- spec/features/switching_turns_spec.rb
+++ spec/features/switching_turns_spec.rb
@@ -4,7 +4,7 @@ feature 'Switching turns ' do
 	context 'Seeing the current turn' do
 		scenario 'At game initialization' do
 			sign_in_and_play
-			expect(page).to have_content("Pikachu's Turn")
+			expect(page).to have_content("Johnny Bravo's Turn")
 		end
 
 	end
